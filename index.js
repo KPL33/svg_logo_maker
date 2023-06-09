52bd06498835b9e9e9199050c4f1923b16933e9f
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-06-09 16:33:17 -0700

    bug fixed

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 const inquirer = require('inquirer');
 
-//Here, we 'require' the 'fs module', which provides built-in functions that will allow us to 'read' the user's inputs and 'write' a saved .svg file from them.
+//Here, we 'require' the 'fs module', which provides built-in functions that will allow us to 'read' the user's inputs and 'write' a saved svg file from them.
 const fs = require('fs');
 
-//Here, we 'require' (import) the accompanying 'generateLogo'.js file, which stores the user's inputs and 'generate's the content of the user's 'svg logo'.
+//Here, we 'require' (import) the accompanying 'generateLogo'.js file, which stores (and styles) the user's inputs and 'generate's the content of the user's 'svg logo'.
 const generateLogo = require('./utils/generateLogo');
 
 //Here, we declare an array of 'questions' that will prompt the user for — and store — their 'input'. Most are standard 'input' prompts, but because we want to limit the user's input to 1 of 3 choices for the 'logoShape', we assign that question a 'type' of 'list'.
@@ -36,9 +36,8 @@ const questions = [
 
 //Here, we declare a 'function' that 'write's the 'logo.svg.' file, based on the 'logoContent' provided by the user via prompts.
 function writeLogo(logoContent) {
-    const svgContent = `<svg>${logoContent}</svg>`;
-    //This 'function' utilizes the 'fs' module's built-in 'writeFile' function to 'write' the 'logoContent' to a .svg file. We also include 'err' as a possible paramater, in case any error should occur during the writing process.
-    fs.writeFile('logo.svg', svgContent, (err) => {
+    //This 'function' utilizes the 'fs' module's built-in 'writeFile' function to 'write' the 'logoContent' to an svg file. We also include 'err' as a possible paramater, in case any error should occur during the writing process.
+    fs.writeFile('logo.svg', logoContent, (err) => {
         //'if' an 'err'or occurs, the user is notified of this through a 'console' 'log'.
         if (err) {
             console.error('Error writing logo.svg file:', err);
@@ -49,7 +48,7 @@ function writeLogo(logoContent) {
     });
 }
 
-//Here, we declare an 'async'hronous 'function' that 'await's the return of the user's 'responses' to the 'prompt's before executing the remaining code. In this way, we ensure that we can 'generate' the 'logoContent' and 'write' the 'Logo' file that will include all of the user-proivded 'responses'.
+//Here, we declare an 'async'hronous 'function' that 'await's the return of the user's 'responses' to the 'prompt's before executing the remaining code. In this way, we ensure that we can 'generate' the 'Markdown' and 'write' the 'README' file that will include all of the user-proivded 'responses'.
 async function init() {
     const responses = await inquirer.prompt(questions);
     console.log(responses);
@@ -58,5 +57,5 @@ async function init() {
 
 }
 
-//Once we have acquired all of the necessary components with the 'init' function, we call it and our 'SVG' file generates in our root folder.
+//Once we have acquired all of the necessary components with the 'init' function, we call it and our 'README' file generates in our root folder.
 init();
--- utils/generateLogo.js
+++ utils/generateLogo.js
@@ -1,27 +1,30 @@
-const { Shape, Triangle, Circle, Square } = require('../lib/shapes');
+const { Triangle, Circle, Square } = require('../lib/shapes');
 
 //Here, we create a function that 'switch'es the 'shape' ('case') of the logo, depending on what the user selects.
 function getShape(logoShape) {
   switch (logoShape) {
-    case 'Triangle':
+    case 'Triangle': 
       return new Triangle();
-    case 'Circle':
+    case 'Circle': 
       return new Circle();
-    case 'Square':
+    case 'Square': 
       return new Square();
-    default:
-      return new Shape();
   }
 }
 
+
+//Here, we declare a 'function' that 'generate's the 'Logo' language for the 'README', based on the user's 'responses'. We have also styled the content, via built-in markdown elements. The information gathered here is what renders in our template.
 function generateLogo(responses) {
   const { logoShape, logoColor, logoText, textColor } = responses;
   const shape = getShape(logoShape);
   shape.setColor(logoColor);
-  shape.setText(logoText);
-  shape.setTextColor(textColor);
-  const logoSVG = shape.render();
-  return logoSVG;
+
+  return `
+  ${shape.render()}
+  ${logoColor}
+  ${logoText}
+  ${textColor}
+`;
 }
 
 //Here we 'export' this js document so that it may be referenced by the 'index.js'.
