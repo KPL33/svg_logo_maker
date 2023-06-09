0c805879da712b5a82bf182efb80c3ae8e5b4517
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-06-09 13:22:24 -0700

    user prompts added, svg file generates

--- index.js
+++ index.js
@@ -1,31 +1,61 @@
-//To generate the logo and to run tests on the shape parameters chosen by the user that will verify that the user has created a logo that meets our acceptance criteria, we have 'init'ialized 'npm inquirer 8.2.4. This has generated our 'package.json' file in the root of our repository. Here, we 'require' that 'inquirer' package, which we'll utilize to 'prompt' the user for their input. 
+//We have 'init'ialized 'npm inquirer 8.2.4 for this challenge. Here, we 'require' the 'inquirer' package, which we'll utilize to 'prompt' the user to provide their preferences on various aspects of the logo. This 'init' also generated our 'package.json' file in the root folder, which enables us to run tests on the generated logo and verify that it meets our acceptance criteria.
+
 const inquirer = require('inquirer');
 
-//Here, we 'require' (import) the accompanying 'generateLogo'.js file. That file stores (and styles) the user's inputs and 'generate's the content of the user's 'svg logo'.
+//Here, we 'require' the 'fs module', which provides built-in functions that will allow us to 'read' the user's inputs and 'write' a saved svg file from them.
+const fs = require('fs');
+
+//Here, we 'require' (import) the accompanying 'generateLogo'.js file, which stores (and styles) the user's inputs and 'generate's the content of the user's 'svg logo'.
 const generateLogo = require('./utils/generateLogo');
 
-//Here, we declare an array of 'questions' (objects) that will prompt the user for — and store — their 'input'. Most are standard 'input' prompts, but because we want to limit the user's input to 1 of 4 choices (for this example), we assign that question regarding the 'license' a 'type' of 'list'. 
+//Here, we declare an array of 'questions' that will prompt the user for — and store — their 'input'. Most are standard 'input' prompts, but because we want to limit the user's input to 1 of 3 choices for the 'logoShape', we assign that question a 'type' of 'list'.
+
 const questions = [
     {
         type: 'list',
-        name: 'logo shape',
+        name: 'logoShape',
         message: 'What shape of logo would you like?',
-        choices: ["Triangle", "Circle", "Square", "None"
-        ]
+        choices: ["Triangle", "Circle", "Square"]
     },
     {
         type: 'input',
-        name: 'logo color',
-        message: 'What color would you like your logo to be? Please enter a hexidecimal value. If you are unfamilir with choosing colors based on hexidecimal value, please visit this website for help: {.href [cli homepage](https://https://htmlcolorcodes.com/color-picker/)}'
+        name: 'logoColor',
+        message: 'Please enter a hexidecimal value for you logo-color. If you are unfamilir with choosing colors based on hexidecimal value, please visit this website for help: https://https://htmlcolorcodes.com/color-picker/'
     },
     {
         type: 'input',
-        name: 'logo text',
+        name: 'logoText',
         message: 'What text would you like to appear on your logo (please limit to 1 to 3 characters)?'
     },
     {
         type: 'input',
-        name: 'text color',
-        message: 'What color would you like the text in your logo to be? Please enter a hexidecimal value. If you are unfamilir with choosing colors based on hexidecimal value, please visit this website for help: {.href [cli homepage](https://https://htmlcolorcodes.com/color-picker/)}'
+        name: 'textColor',
+        message: 'Please enter a hexidecimal value for the text-color. If you are unfamilir with choosing colors based on hexidecimal value, please visit this website for help: https://https://htmlcolorcodes.com/color-picker/'
     }
 ];
+
+//Here, we declare a 'function' that 'write's the 'logo.svg.' file, based on the 'logoContent' provided by the user via prompts.
+function writeLogo(logoContent) {
+    //This 'function' utilizes the 'fs' module's built-in 'writeFile' function to 'write' the 'logoContent' to an svg file. We also include 'err' as a possible paramater, in case any error should occur during the writing process.
+    fs.writeFile('logo.svg', logoContent, (err) => {
+        //'if' an 'err'or occurs, the user is notified of this through a 'console' 'log'.
+        if (err) {
+            console.error('Error writing logo.svg file:', err);
+            //Otherwise, the user is notified that the operation was successful.
+        } else {
+            console.log('logo.svg file generated successfully!');
+        }
+    });
+}
+
+//Here, we declare an 'async'hronous 'function' that 'await's the return of the user's 'responses' to the 'prompt's before executing the remaining code. In this way, we ensure that we can 'generate' the 'Markdown' and 'write' the 'README' file that will include all of the user-proivded 'responses'.
+async function init() {
+    const responses = await inquirer.prompt(questions);
+    console.log(responses);
+    const logoContent = generateLogo(responses);
+    writeLogo(logoContent);
+
+}
+
+//Once we have acquired all of the necessary components with the 'init' function, we call it and our 'README' file generates in our root folder.
+init();
--- utils/generateLogo.js
+++ utils/generateLogo.js
@@ -1,90 +1,25 @@
-//Here, we create a function that 'switch'es the 'license' badge that will display in the 'README', based on which 'license' ('case') is selected by the user.
-function renderLicenseBadge(license) {
-  switch (license) {
-    case 'MIT': 
-      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
-    case 'Apache': 
-      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
-    case 'Eclipse': 
-      return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
-    default:
-      return 'None'
+//Here, we create a function that 'switch'es the 'shape' ('case') of the logo, depending on what the user selects.
+function getShape(logoShape) {
+  switch (logoShape) {
+    case 'Triangle': 
+      return new Triangle();
+    case 'Circle': 
+      return new Circle();
+    case 'Square': 
+      return new Square();
   }
 }
 
 
-//Here, we declare a 'function' that 'generate's 'markdown' language for the 'README', based on the user's 'responses'. We have also styled the content, via built-in markdown elements. The information gathered here is what renders in our template.
-function generateMarkdown(responses) {
-  const title = `<span style="color: yellow;">***${responses.title}***</span>`;
-  return `# ${title}
-
-  ${renderLicenseBadge(responses.license)}
-
-  ## Description
-  ${responses.description}
-  
-  <br />
-
-  ***
-
-  ## Table of Contents
-  1. [Installation](#installation)
-  2. [Usage](#usage)
-  3. [License](#license)
-  4. [Contributing](#contributing)
-  5. [Tests](#tests)
-  6. [GitHub](#github)
-  7. [Email](#email)
-  
-  ***
-
-  <br />
-  
-  <section id="installation"/>
-  
-  ## Installation Instructions
-  ${responses.installation}
-
-  <br />
-  <section id="usage"/>
-
-  ## Usage
-  ${responses.usage}
-
-  <br />
-  <section id="license"/>
-
-  ## Licenses
-  This application is covered under the following license (for a link to more info. about this license, please click the badge at top of this README):
-  
-  ${responses.license}
-
-  <br />
-  <section id="contributing"/>
-
-  ## Contributors To This Application
-  ${responses.contributing}
-
-  <br />
-  <section id="tests"/>
-
-  ## Testing Instructions
-  ${responses.tests}
-
-  <br />
-  <section id="github"/>
-
-  ## Questions? Contact the Developer on GitHub... 
-  ${responses.github}
-
-  <br />
-  <section id="email"/>
-
-  ## ...or via Email
-  ${responses.email}
-
+//Here, we declare a 'function' that 'generate's the 'Logo' language for the 'README', based on the user's 'responses'. We have also styled the content, via built-in markdown elements. The information gathered here is what renders in our template.
+function generateLogo(responses) {
+  const logoShape = `${responses.logoShape}`;
+  return `${logoShape}
+  ${responses.logoColor}
+  ${responses.logoText}
+  ${responses.textColor}
 `;
 }
 
 //Here we 'export' this js document so that it may be referenced by the 'index.js'.
-module.exports = generateMarkdown;
+module.exports = generateLogo;
