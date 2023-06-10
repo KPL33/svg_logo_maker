85d9daa92fe373e8778ad1cdae48fcfcc1203aa4
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2023-06-09 20:53:56 -0700

    text generates within logo

--- utils/generateLogo.js
+++ utils/generateLogo.js
@@ -19,10 +19,19 @@ function generateLogo(responses) {
   const shape = getShape(logoShape);
   shape.setColor(logoColor);
 
+  let transform;
+  if (logoShape === 'Triangle') {
+    transform = 'translate(125 140)'; // Adjust translation for triangle shape
+  } else if (logoShape === 'Circle') {
+    transform = 'translate(25 75)'; // Adjust translation for circle shape
+  } else if (logoShape === 'Square') {
+    transform = 'translate(25 25)'; // Adjust translation for square shape
+  }
+
   return `<svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
   ${shape.render()}
-  <text x="100" y="200" fill="${textColor}" font-size="20" text-anchor="middle">${logoText}</text>
-  </svg>`;
+  <text fill="${textColor}" font-size="20" text-anchor="middle" transform="${transform}">${logoText}</text>
+</svg>`;
 }
 
 //Here we 'export' this js document so that it may be referenced by the 'index.js'.
